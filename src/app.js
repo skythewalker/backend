f838e2b72c13e1360200bb287039485fe65d86e6
AKASH SHINDE <akashnitin.shinde@hitachivantara.com>
2023-12-19 17:09:09 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -9,6 +9,14 @@ app.use(cors({
      credentials: true
 }))
 
+
+//routes import
+import userRouter from './routes/user.routes.js'
+
+//routes decleration
+//http://localhost:8000/api/v1/users/register
+app.use("/api/v1/users", userRouter);
+
 app.use(express.json({limit: '16kb'}));
 app.use(express.urlencoded({extended: true, limit: '16kb'}));
 app.use(express.static('public'));
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localPath) => {
+  try {
+    if (!localPath) return null;
+    //upload the file on cloudinary
+    const res =  await cloudinary.uploader.upload(localPath, {
+      resource_type: 'auto',
+    });
+    //file has been uploaded successfully
+    console.log('File is uploaded on cloudinary', res.url);
+    return res;
+  } catch (error) {
+      fs.unlinkSync(localPath) //remove the locally saved temp file as the upload operation got failed
+      return null;
+  }
+};
+
+
+export {uploadOnCloudinary}
